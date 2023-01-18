4adc9a67b5969025a4239fa1e0af10c6a1ce0599
rickt25 <derricktio36@gmail.com>
2023-01-18 21:44:38 +0700

    Reformat activity response

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -19,14 +19,31 @@ class Activity extends Model
         return $this->category->name;
     }
 
+    public function wallet(){
+        return $this->belongsTo(Wallet::class);
+    }
+
+    public function getWalletNameAttribute(){
+        return $this->wallet->wallet_name;
+    }
+
     public function category(){
         return $this->belongsTo(Category::class);
     }
 
-    // public function toArray()
-    // {
-    //     $array = parent::toArray();
-    //     $array['category_name'] = $this->category_name;
-    //     return $array;
-    // }
+    public function toArray()
+    {
+        $array = parent::toArray();
+        $array['category_name'] = $this->category_name;
+        $array['wallet_name'] = $this->wallet_name;
+
+        // format activity_date dd-mm-yyyy
+        $array['activity_date'] = $this->activity_date->format('d-m-Y');
+
+        unset($array['user_id']);
+        unset($array['wallet_id']);
+        unset($array['category_id']);
+
+        return $array;
+    }
 }
